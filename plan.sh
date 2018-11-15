b214672be9fe53e1b3a71c385f1aec119fab9e4b
Steven Danna <steve@chef.io>
2018-11-15 10:07:19 +0000

    Rewrite in Go to avoid runtime deps
    Depending on busybox-static means that any service that depends on
    chef/mlsa will have busybox in their path. This PR replaces the shell
    script with a small Go program that should be equivalent.
    
    One bummer is that the go binary is actually pretty large even after
    stripping. One alternative would be to just use C, but this seemed
    more easily maintainable by a broader group of Chef developers over
    time.
    
    Signed-off-by: Steven Danna <steve@chef.io>

--- plan.sh
+++ plan.sh
@@ -6,14 +6,13 @@ pkg_upstream_url="https://www.chef.io/online-master-agreement"
 pkg_maintainer="Chef Software Inc."
 pkg_license=('Proprietary')
 pkg_bin_dirs=(bin)
-pkg_deps=(
-  core/busybox-static
+pkg_build_deps=(
+  core/go
 )
 do_build() {
-  return 0
+  CGO_ENABLED=0 go build -ldflags '-s -w -extldflags "-static"' accept.go
 }
 
 do_install() {
   install -m 0755 "${SRC_PATH}/accept" "${pkg_prefix}/bin"
-  fix_interpreter "${pkg_prefix}/bin/accept" core/busybox-static bin/env
 }
